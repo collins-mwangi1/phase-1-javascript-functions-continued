dd8791d63d5a308711b755ccc4342ad7264562fd
collins-mwangi1 <collins.mwangi1@student.moringaschool.com>
2024-03-28 21:32:51 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(act = 'roller-skate'){
+    let acti = `This saturday, i want to ${act}!`
+    return acti
+}
+
+
+//saturdayFun//
+function mondayWork(act = 'go to the office'){
+    let acti = `This mondayWork, i will ${act}.`
+    return acti
+}
+
+function wrapAdjective(emph="*"){
+    return function (msg ="special"){
+        return `yu are ${emph}${msg}${emph}!`
+    }
+}
